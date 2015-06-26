763df250cbcd56d5de3493c808bc15e119ff87a9
JCabero <j.c.revolution@hotmail.com>
2015-06-27 00:09:46 +0200

    Improved ListLinker

--- core/src/com/caberodev/squarearmy/world/ListLinker.java
+++ core/src/com/caberodev/squarearmy/world/ListLinker.java
@@ -8,24 +8,62 @@ import java.util.HashMap;
  * @author Javier Cabero Guerra <br>
  * 
  * Copyright 2015 (c) All Rights Reserved. <br><br>
+ * 
+ * Allows associations of lists under key names.
  *       
  */
 public class ListLinker {
 
-	private static HashMap<String, ArrayList<WorldObject>> linker;
+	private static HashMap<String, ArrayList<Object>> linker = new HashMap<String, ArrayList<Object>>();
 	
-	public static void add(WorldObject wo) {
-		ArrayList<WorldObject> list = linker.get(wo.getClass().getName());
+	/**
+	 * Adds the given Object to its list.
+	 */
+	public static void add(Object o) {
+		String			   key = o.getClass().getName();
+		ArrayList<Object> list = linker.get(key);
 		if(list == null) {
-			list = new ArrayList<WorldObject>();
-			linker.put(wo.getClass().getName(), list);
+			list = new ArrayList<Object>();
+			linker.put(key, list);
 		}
-		list.add(wo);
+		list.add(o);
 	}
 	
-	public static void del(WorldObject wo) {
-		ArrayList<WorldObject> list = linker.get(wo.getClass().getName());
+	/**
+	 * Adds the given Object to key list.
+	 */
+	public static void add(String key, Object o) {
+		ArrayList<Object> list = linker.get(key);
+		if(list == null) {
+			list = new ArrayList<Object>();
+			linker.put(key, list);
+		}
+		list.add(o);
+	}
+	
+	/**
+	 * Deletes the given Object from its list.
+	 */
+	public static void del(Object o) {
+		String			   key = o.getClass().getName();
+		ArrayList<Object> list = linker.get(key);
 		if(list != null) 
-			list.remove(wo);
+			list.remove(o);
+	}
+	
+	/**
+	 * Deletes the given Object from key list.
+	 */
+	public static void del(String key, Object o) {
+		ArrayList<Object> list = linker.get(key);
+		if(list != null) 
+			list.remove(o);
+	}
+	
+	/**
+	 * Returns the Objects list associated with the given key. If such association doesn't exists returns null. 
+	 */
+	public static ArrayList<Object> get(String key) {
+		return linker.get(key);
 	}
 }
