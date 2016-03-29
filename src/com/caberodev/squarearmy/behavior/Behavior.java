628a1e74414e30caeb5a53326a618b6707a7c594
JCabero <j.c.revolution@hotmail.com>
2016-03-29 23:51:54 +0200

    Behavior class added (it was not added in older commits)

---
+++ src/com/caberodev/squarearmy/behavior/Behavior.java
@@ -0,0 +1,10 @@
+package com.caberodev.squarearmy.behavior;
+
+import com.caberodev.squarearmy.components.Component;
+
+public class Behavior extends Component {
+
+	public Behavior() {
+		super("behavior");
+	}
+}
